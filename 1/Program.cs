0cd648c846c1806c67e544bd2a4a95735e1d66de
Lili-Zd <zdanova.liliya@gmail.com>
2022-12-12 23:01:49 +0300

    Dobablena papka 1

---
+++ 1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Reverse(int number) {
+    int answer = 0;
+    while (number > 0) {
+        answer = answer * 10 + number % 10;
+        number /= 10;
+    }
+    return answer;
+}
+
+
+bool ValidateNumber(int num)
+{
+    if(num >=10000 && num < 100000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Введенное число не пятизначное");
+    return false;
+}
+
+bool IsPalindrom(int num)
+{
+    return num == Reverse(num);    
+}
+
+int number = Prompt("Введите пятизначное число");
+if(IsPalindrom(number))
+    {
+        System.Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number} не является палиндромом");
+    }
